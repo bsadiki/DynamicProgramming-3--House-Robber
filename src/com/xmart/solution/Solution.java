fedbbae45cd6a064e97b7e2d24bf02a3826c391b
bsadiki <pedrosadiki@gmail.com>
2019-06-25 14:59:11 +0100

    naive solution : Recursive

--- src/com/xmart/solution/Solution.java
+++ src/com/xmart/solution/Solution.java
@@ -2,6 +2,16 @@ package com.xmart.solution;
 
 public class Solution {
     public static void main(String[] args) {
+        int[] housesValues = {1, 8, 3, 4, 9};
+        System.out.println(maxRobbed(housesValues, 5));
+    }
+
+    private static int maxRobbed(int[] housesValues, int n) {
+        if (n == 1)
+            return housesValues[0];
+        if (n == 2)
+            return housesValues[1];
+        return Math.max(housesValues[n - 1] + maxRobbed(housesValues, n - 2), maxRobbed(housesValues, n - 1));
     }
 
 }
